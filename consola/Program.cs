277957d41d4a1a97a56117ac5b1c4341511321bd
Rafael Bolanos Herrera <ra.bolanos@tcs.com>
2025-04-27 10:05:14 -0500

    ajustes visuales en consola

--- consola/Program.cs
+++ consola/Program.cs
@@ -4,9 +4,10 @@ Console.WriteLine("\nSuma de dos valores");
 Console.WriteLine("\nDigite el valor de a: ");
 int a = int.Parse(Console.ReadLine());
 
-Console.WriteLine("\nDigite el valor de b: ");
+Console.WriteLine("Digite el valor de b: ");
 int b = int.Parse(Console.ReadLine());
 
+Console.WriteLine("\nResultado: ");
 Console.WriteLine(new Calculator().Sum(a,b));
 
-Console.WriteLine("\nPresione cualquier tecla para terminar...");
+Console.WriteLine("\nAplicacion finalizada");
